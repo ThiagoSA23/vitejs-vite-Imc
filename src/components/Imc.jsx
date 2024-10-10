985793a90939c258466f6c14cc87e260fb6efc25
ThiagoSA23 <thiago.azevedo@iesb.edu.br>
2024-10-10 18:26:00 -0300

    commit red

--- src/components/Imc.jsx
+++ src/components/Imc.jsx
@@ -4,11 +4,24 @@ const Imc = () => {
   const [peso, setPeso] = useState();
   const [altura, setAltura] = useState();
   const [resultado, setResultado] = useState();
+  const [mensagem, setMensagem] = useState();
   const calcImc = () => {
     const Metro = altura / 100;
     const imc = peso / Metro ** 2;
     const arredondar = imc.toFixed(2);
     setResultado(arredondar);
+
+    let msg = '';
+        if (imc < 18.5) {
+            msg = 'Abaixo do peso';
+        } else if (imc >= 18.5 && imc < 24.9) {
+            msg = 'Peso normal';
+        } else if (imc >= 25 && imc < 29.9) {
+            msg = 'Sobrepeso';
+        } else {
+            msg = 'Obesidade';
+        }
+        setMensagem(msg);
   };
   return (
     <div>
@@ -20,6 +33,7 @@ const Imc = () => {
         placeholder="Peso(Kg)"
         required
       />
+      
       <input
         type="number"
         value={altura}
@@ -27,8 +41,10 @@ const Imc = () => {
         placeholder="Altura(M)"
         required
       />
+
       <button onClick={calcImc}>Calcular</button>
-      {resultado}
+      <h2>{resultado}</h2>
+      <h3>{mensagem}</h3>
     </div>
   );
 };
